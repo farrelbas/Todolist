caf233798a83451be09e802d608e7c7e623bf6f3
farrelbuzz067 <farrel_baswara_28rpl@student.smktelkom-mlg.sch.id>
2022-04-22 10:26:04 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,83 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Models\Task;
+use Illuminate\Support\Facades\Validator;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/', function () {
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
+});
+
+
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+
+    return redirect('/');
+});
+
+
+Route::delete('/task/{task}', function (Task $task) {
+    $task->delete();
+
+    return redirect('/');
+});
+
+
+// Route::group(['middleware' => ['web']], function () {
+
+//     Route::get('/', function () {
+//         return view('tasks');
+//     });
+
+//     Route::post('/task', function (Request $request) {
+//         $validator = Validator::make($request->all(), [
+//             'name' => 'required|max:255',
+//         ]);
+
+//         if ($validator->fails()) {
+//             return redirect('/')
+//                 ->withInput()
+//                 ->withErrors($validator);
+//         }
+
+//         $task = new Task;
+//         $task->name = $request->name;
+//         $task->save();
+
+//         return redirect('/');
+//     });
+
+//     Route::delete('/task/{id}', function ($id) {
+//         Task::findOrFail($id)->delete();
+
+//         return redirect('/');
+//     });
+// });
